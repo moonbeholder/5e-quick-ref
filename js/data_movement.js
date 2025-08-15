6320332d3f65b3bc55c3b6be715efee60a69445f
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-15 23:39:51 +0900

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -6,53 +6,52 @@ data_movement = [
         description: "Movement cost: 5ft per 5ft moved",
         reference: "???",
         bullets: [
-            "If you have more than one speed, such as your walking speed and a flying speed, you can switch back and forth between your speeds during your move. Whenever you switch, subtract the distance you've already moved from the new speed.",
-            "You can move through a nonhostile creature's space.",
-            "You can move through a hostile creature's space only if the creature is at least two sizes larger or smaller than you.",
-            "Another creature's space is difficult terrain for you.",
-            "Whether a creature is a friend or an enemy, you can't willingly end your move in its space."
+            "On your turn, you can move a distance equal to your Speed or less. Or you can decide not to move.",
+            "Your movement can include climbing, crawling, jumping, and swimming (each explained in the rules glossary). These different modes of movement can be combined with your regular movement, or they can constitute your entire move.",
+            "However you’re moving with your Speed, you deduct the distance of each part of your move from it until it is used up or until you are done moving, whichever comes first.",
+            "<b><i>Moving around Other Creatures.</i></b> During your move, you can pass through the space of an ally, a creature that has the Incapacitated condition, a Tiny creature, or a creature that is two sizes larger or smaller than you. Another creature’s space is Difficult Terrain for you unless that creature is Tiny or your ally. You can’t willingly end a move in a space occupied by another creature. If you somehow end a turn in a space with another creature, you have the Prone condition unless you are Tiny or are of a larger size than the other creature."
         ]
     },
     {
         title: "Climb",
         icon: "mountain-climbing",
         subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft climbed",
-        reference: "PHB, pg. 182.",
+        description: "Move vertically. Movement cost: 10ft per 5ft climbed",
+        reference: "???",
         bullets: [
-            "May involve a Strength (Athletics) check if the climb is difficult"
+            "While you’re climbing, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain). You ignore this extra cost if you have a Climb Speed and use it to climb.",
+            "At the DM’s discretion, climbing a slippery surface or one with few handholds might require a successful DC 15 Strength (Athletics) check."
         ]
     },
     {
         title: "Swim",
         icon: "at-sea",
         subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft swum",
-        reference: "PHB, pg. 182.",
+        description: "Move through water. Movement cost: 10ft per 5ft swum",
+        reference: "???",
         bullets: [
-            "May involve a Strength (Athletics) check if the swim is difficult"
+            "While you’re swimming, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain). You ignore this extra cost if you have a Swim Speed and use it to swim.",
+            "At the DM’s discretion, moving any distance in rough water might require a successful DC 15 Strength (Athletics) check."
         ]
     },
     {
-        title: "Drop prone",
+        title: "Drop Prone",
         icon: "falling",
-        subtitle: "Cost: 0ft",
-        description: "Movement cost: 0ft (free)",
-        reference: "PHB, pgs. 190-191,292.",
+        subtitle: "Cost: 0ft.",
+        description: "Drop to the ground. Movement cost: 0ft.",
+        reference: "???",
         bullets: [
-            "You can drop prone without using any of your speed",
-            "To move while prone, you must crawl or use magic such as teleportation",
-            "Dropping prone adds the <i>Prone</i> condition (melee attacks against you have advantage, ranged attacks against you have disadvantage, your own attacks have disadvantage)"
+            "On your turn, you can give yourself the Prone condition without using an action or any of your Speed, but you can't do so if your speed is 0."
         ]
     },
     {
         title: "Crawl",
         icon: "crawl",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Movement cost: 10ft per 5ft crawled",
-        reference: "PHB, pg. 182.",
+        subtitle: "Cost: 10ft per 5ft.",
+        description: "Movement cost: 10ft per 5ft crawled.",
+        reference: "???",
         bullets: [
-
+            "While you’re crawling, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain)."
         ]
     },
     {
@@ -60,36 +59,33 @@ data_movement = [
         icon: "strong",
         subtitle: "Cost: half movement speed",
         description: "Movement cost: half of your speed",
-        reference: "PHB, pg. 190-191.",
+        reference: "???",
         bullets: [
             "You can't stand up if you don't have enough movement left or if your speed is 0"
         ]
     },
     {
-        title: "High jump",
+        title: "High Jump",
         icon: "wingfoot",
         subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft jumped",
+        description: "Jump upwards into the air.",
         
-        reference: "PHB, pg. 182.",
+        reference: "???",
         bullets: [
-            "You leap into the air a number of feet equal to <b>3 + your Strength modifier</b> if you move at least 10 feet on foot immediately before the jump.",
-            "When you make a standing high jump, you can jump only half that distance.",
-            "You can extend your arms half your height above yourself during the jump.",
-            "In some circumstances, your DM might allow you to make a Strength (Athletics) check to jump higher than you normally can."
+            "When you make a High Jump, you leap into the air a number of feet equal to 3 plus your Strength modifier (minimum of 0 feet) if you move at least 10 feet on foot immediately before the jump. When you make a standing High Jump, you can jump only half that distance. Either way, each foot of the jump costs a foot of movement.",
+            "You can extend your arms half your height above yourself during the jump. Thus, you can reach a distance equal to the height of the jump plus 1.5 times your height."
         ]
     },
     {
-        title: "Long jump",
-        icon: "wingfoot",
+        title: "Long Jump",
+        icon: "jump-across",
         subtitle: "Cost: 5ft per 5ft",
         description: "Movement cost: 5ft per 5ft jumped",
-        reference: "PHB, pg. 182.",
+        reference: "???",
         bullets: [
-            "You cover a number of feet up to your <b>Strength score</b> if you move at least 10 feet on foot immediately before the jump.",
-            "When you make a standing long jump, you can leap only half that distance",
-            "May involve a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance). You hit the obstacle on a failed check.",
-            "May involve a DC 10 Dexterity (Acrobatics) check to land on your feet in difficult terrain. You land prone on a failed check."
+            "When you make a Long Jump, you leap horizontally a number of feet up to your Strength score if you move at least 10 feet immediately before the jump. When you make a standing Long Jump, you can leap only half that distance. Either way, each foot you jump costs a foot of movement.",
+            "If you land in Difficult Terrain, you must succeed on a DC 10 Dexterity (Acrobatics) check or have the Prone condition.",
+            "This Long Jump rule assumes that the height of the jump doesn't matter, such as a jump across a stream or a chasm. At your DM's discretion, you must succeed on a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance), such as a hedge or a low wall. Otherwise, you hit the obstacle."
         ]
     },
     {
@@ -97,6 +93,7 @@ data_movement = [
         icon: "juggler",
         subtitle: "Any stunt not on this list",
         description: "Perform any movement or stunt you can imagine",
+        reference: "???",
         bullets: [
             "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
         ]
@@ -105,9 +102,11 @@ data_movement = [
         title: "Difficult terrain",
         icon: "stone-pile",
         subtitle: "Cost modifier: +5ft per 5ft",
-        reference: "PHB, pg. 182.",
         description: "Moving in difficult terrain costs an additional 5ft per 5ft of movement",
+        reference: "???",
         bullets: [
+            "If a space is Difficult Terrain, every foot of movement in that space costs 1 extra foot. For example, moving 5 feet through Difficult Terrain costs 10 feet of movement. Difficult Terrain isn't cumulative; either a space is Difficult Terrain, or it isn't.",
+            "A space is Difficult Terrain if it contains any of the following, or something similar:\n- A creature that isn't Tiny or your ally.\n- Furniture that is sized for creatures of your size or larger.\n- Heavy snow, ice, rubble, or undergrowth.\n- Liquid that's between shin- and waist-deep.\n- A narrow opening sized for a creature one size smaller than you.\n- A slope of 20 degrees or more."
         ]
     },
     {
@@ -115,7 +114,7 @@ data_movement = [
         icon: "grab",
         subtitle: "Modifier: speed halved",
         description: "Drag or carry the grappled creature with you",
-        reference: "PHB, pg. 195.",
+        reference: "???",
         bullets: [
             "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
             "See the attack action for how to grapple a creature."
