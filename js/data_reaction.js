be127a54ee0b8f467aec64bb8f0f79860e91f9a2
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-16 18:16:31 +0900

    Update data_reaction.js

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,36 +1,35 @@
 data_reaction = [
     {
-        title: "Opportunity attack",
-        icon: "crossed-swords",
+        title: "Opportunity Attack",
+        icon: "sword-brandish",
         subtitle: "Enemy leaves your reach",
-        description: "You can rarely move heedlessly past your foes without putting yourself in danger",
-        reference: "PHB, pg. 195.",
+        description: "Combatants watch for enemies to drop their guard. Moving heedlessly past foes, puts you in danger by provoking an Opportunity Attack.",
+        reference: "See Playing the Game: Combat (Melee Attacks), Rules Glossary: Opportunity Attacks",
         bullets: [
-            "Trigger: enemy creature you can see leaves your reach.",
-            "Make one melee attack against the provoking creature.",
-            "The attack interrupts the provoking creature's movement, occurring right before the creature leaves your reach.",
-            "Creatures don't provoke an opportunity attack when they teleport or when someone or something moves them without using their movement, action, or reaction."
+            "You can make an Opportunity Attack when a creature that you can see leaves your reach using its action, its Bonus Action, its Reaction, or one of its speeds. To make the Opportunity Attack, take a Reaction to make one melee attack with a weapon or an Unarmed Strike against the provoking creature. The attack occurs right before the creature leaves your reach.",
+            "<b><i>Avoiding Opportunity Attacks.</i></b> You can avoid provoking an Opportunity Attack by taking the Disengage action. You also don’t provoke an Opportunity Attack when you teleport or when you are moved without using your movement, action, Bonus Action, or Reaction. For example, you don’t provoke an Opportunity Attack if an explosion hurls you out of a foe’s reach or if you fall past an enemy.",
+            "<b><i>Making an Opportunity Attack.</i></b> You can make an Opportunity Attack when a creature that you can see leaves your reach. To make the attack, take a Reaction to make one melee attack with a weapon or an Unarmed Strike against that creature. The attack occurs right before it leaves your reach."
         ]
     },
     {
-        title: "Readied action",
+        title: "Readied Action",
         icon: "stopwatch",
-        subtitle: "Part of your Ready action",
-        description: "Execute the reaction specified by your Ready action",
-        reference: "PHB, pg. 193.",
+        subtitle: "Do the readied Action.",
+        description: "Execute the reaction specified by your Ready action, in response to the chosen trigger.",
+        reference: "See Rules Glossary: Ready [Action]",
         bullets: [
-            "Trigger: specified by your <i>Ready</i> action."
+            "When the trigger occurs, you can either take your Reaction right after the trigger finishes or ignore the trigger.",
+            "<b><i>Readying a Spell.</i></b> When you Ready a spell, you cast it as normal (expending any resources used to cast it) but hold its energy, which you release with your Reaction when the trigger occurs. To be readied, a spell must have a casting time of an action, and holding on to the spell’s magic requires Concentration, which you can maintain up to the start of your next turn. If your Concentration is broken, the spell dissipates without taking effect."
         ]
     },
     {
-        title: "Cast a spell",
+        title: "Cast a Spell",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 reaction",
-        description: "Cast a spell with a casting time of 1 reaction",
-        reference: "PHB, pg. 192.",
+        subtitle: "Cast a spell as a reaction.",
+        description: "Cast a spell with a casting time of 1 reaction.",
+        reference: "See Spells: Casting Spells (Casting Time)",
         bullets: [
-            "Trigger: specified by the spell.",
-            "For further details, see the <i>Cast a spell</i> action."
+            "<b><i>Reaction Triggers.</i></b> A spell that has a casting time of a Reaction is cast in response to a trigger that is defined in the spell’s Casting Time entry."
         ]
     }
 ]
