18a56eb93145ee8045930cffcb5f317fc2bf989c
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-16 10:08:46 +0900

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -106,7 +106,13 @@ data_movement = [
         reference: "???",
         bullets: [
             "If a space is Difficult Terrain, every foot of movement in that space costs 1 extra foot. For example, moving 5 feet through Difficult Terrain costs 10 feet of movement. Difficult Terrain isn't cumulative; either a space is Difficult Terrain, or it isn't.",
-            "A space is Difficult Terrain if it contains any of the following, or something similar:\n- A creature that isn't Tiny or your ally.\n- Furniture that is sized for creatures of your size or larger.\n- Heavy snow, ice, rubble, or undergrowth.\n- Liquid that's between shin- and waist-deep.\n- A narrow opening sized for a creature one size smaller than you.\n- A slope of 20 degrees or more."
+            "A space is Difficult Terrain if it contains any of the following, or something similar:",
+            "- A creature that isn't Tiny or your ally.",
+            "- Furniture that is sized for creatures of your size or larger.",
+            "- Heavy snow, ice, rubble, or undergrowth.",
+            "- Liquid that's between shin- and waist-deep.",
+            "- A narrow opening sized for a creature one size smaller than you.",
+            "- A slope of 20 degrees or more."
         ]
     },
     {
