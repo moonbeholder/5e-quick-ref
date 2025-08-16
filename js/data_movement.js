29168958bdc754fb42aff3580ed01a5ea10cc659
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-16 16:19:32 +0900

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -1,22 +1,9 @@
 data_movement = [
     {
-        title: "Move",
-        icon: "run",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft moved",
-        reference: "???",
-        bullets: [
-            "On your turn, you can move a distance equal to your Speed or less. Or you can decide not to move.",
-            "Your movement can include climbing, crawling, jumping, and swimming (each explained in the rules glossary). These different modes of movement can be combined with your regular movement, or they can constitute your entire move.",
-            "However you’re moving with your Speed, you deduct the distance of each part of your move from it until it is used up or until you are done moving, whichever comes first.",
-            "<b><i>Moving around Other Creatures.</i></b> During your move, you can pass through the space of an ally, a creature that has the Incapacitated condition, a Tiny creature, or a creature that is two sizes larger or smaller than you. Another creature’s space is Difficult Terrain for you unless that creature is Tiny or your ally. You can’t willingly end a move in a space occupied by another creature. If you somehow end a turn in a space with another creature, you have the Prone condition unless you are Tiny or are of a larger size than the other creature."
-        ]
-    },
-    {
         title: "Climb",
         icon: "mountain-climbing",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Move vertically. Movement cost: 10ft per 5ft climbed",
+        subtitle: "1 foot moved costs 1 extra foot.",
+        description: "Climb up or down something.",
         reference: "???",
         bullets: [
             "While you’re climbing, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain). You ignore this extra cost if you have a Climb Speed and use it to climb.",
@@ -24,85 +11,30 @@ data_movement = [
         ]
     },
     {
-        title: "Swim",
-        icon: "at-sea",
-        subtitle: "Cost: 10ft per 5ft",
-        description: "Move through water. Movement cost: 10ft per 5ft swum",
-        reference: "???",
-        bullets: [
-            "While you’re swimming, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain). You ignore this extra cost if you have a Swim Speed and use it to swim.",
-            "At the DM’s discretion, moving any distance in rough water might require a successful DC 15 Strength (Athletics) check."
-        ]
-    },
-    {
-        title: "Drop Prone",
-        icon: "falling",
-        subtitle: "Cost: 0ft.",
-        description: "Drop to the ground. Movement cost: 0ft.",
-        reference: "???",
-        bullets: [
-            "On your turn, you can give yourself the Prone condition without using an action or any of your Speed, but you can't do so if your speed is 0."
-        ]
-    },
-    {
         title: "Crawl",
         icon: "crawl",
-        subtitle: "Cost: 10ft per 5ft.",
-        description: "Movement cost: 10ft per 5ft crawled.",
+        subtitle: "1 foot moved costs 1 extra foot.",
+        description: "Crawl across the ground.",
         reference: "???",
         bullets: [
             "While you’re crawling, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain)."
         ]
     },
     {
-        title: "Stand up",
-        icon: "strong",
-        subtitle: "Cost: half movement speed",
-        description: "Movement cost: half of your speed",
+        title: "Drop Prone",
+        icon: "falling",
+        subtitle: "Free unless your speed is 0.",
+        description: "Drop to the ground.",
         reference: "???",
         bullets: [
-            "You can't stand up if you don't have enough movement left or if your speed is 0"
+            "On your turn, you can give yourself the Prone condition without using an action or any of your Speed, but you can't do so if your speed is 0."
         ]
     },
     {
-        title: "High Jump",
-        icon: "wingfoot",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Jump upwards into the air.",
-        
-        reference: "???",
-        bullets: [
-            "When you make a High Jump, you leap into the air a number of feet equal to 3 plus your Strength modifier (minimum of 0 feet) if you move at least 10 feet on foot immediately before the jump. When you make a standing High Jump, you can jump only half that distance. Either way, each foot of the jump costs a foot of movement.",
-            "You can extend your arms half your height above yourself during the jump. Thus, you can reach a distance equal to the height of the jump plus 1.5 times your height."
-        ]
-    },
-    {
-        title: "Long Jump",
-        icon: "jump-across",
-        subtitle: "Cost: 5ft per 5ft",
-        description: "Movement cost: 5ft per 5ft jumped",
-        reference: "???",
-        bullets: [
-            "When you make a Long Jump, you leap horizontally a number of feet up to your Strength score if you move at least 10 feet immediately before the jump. When you make a standing Long Jump, you can leap only half that distance. Either way, each foot you jump costs a foot of movement.",
-            "If you land in Difficult Terrain, you must succeed on a DC 10 Dexterity (Acrobatics) check or have the Prone condition.",
-            "This Long Jump rule assumes that the height of the jump doesn't matter, such as a jump across a stream or a chasm. At your DM's discretion, you must succeed on a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance), such as a hedge or a low wall. Otherwise, you hit the obstacle."
-        ]
-    },
-    {
-        title: "Improvise",
-        icon: "juggler",
-        subtitle: "Any stunt not on this list",
-        description: "Perform any movement or stunt you can imagine",
-        reference: "???",
-        bullets: [
-            "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
-        ]
-    },
-    {
-        title: "Difficult terrain",
+        title: "Difficult Terrain",
         icon: "stone-pile",
-        subtitle: "Cost modifier: +5ft per 5ft",
-        description: "Moving in difficult terrain costs an additional 5ft per 5ft of movement",
+        subtitle: "1 foot moved costs 1 extra foot.",
+        description: "Move through any area that isn't easy to traverse.",
         reference: "???",
         bullets: [
             "If a space is Difficult Terrain, every foot of movement in that space costs 1 extra foot. For example, moving 5 feet through Difficult Terrain costs 10 feet of movement. Difficult Terrain isn't cumulative; either a space is Difficult Terrain, or it isn't.",
@@ -118,12 +50,78 @@ data_movement = [
     {
         title: "Grapple move",
         icon: "grab",
-        subtitle: "Modifier: speed halved",
-        description: "Drag or carry the grappled creature with you",
+        subtitle: "Your speed is halved.",
+        description: "Drag or carry a grappled creature with you.",
+        reference: "See Grappled [Condition]",
+        bullets: [
+            "If you are grappling a creature, you can drag or carry the creature with you when you move, but every foot of movement costs 1 extra foot unless that creature is Tiny or two or more sizes smaller than you."
+        ]
+    },
+    {
+        title: "High Jump",
+        icon: "wingfoot",
+        subtitle: "1 foot moved costs 1 foot.",
+        description: "Make a vertical leap into the air.",
         reference: "???",
         bullets: [
-            "If you move while grappling another creature, your speed is halved, unless the creature is two or more sizes smaller than you.",
-            "See the attack action for how to grapple a creature."
+            "When you make a High Jump, you leap into the air a number of feet equal to 3 plus your Strength modifier (minimum of 0 feet) if you move at least 10 feet on foot immediately before the jump. When you make a standing High Jump, you can jump only half that distance. Either way, each foot of the jump costs a foot of movement.",
+            "You can extend your arms half your height above yourself during the jump. Thus, you can reach a distance equal to the height of the jump plus 1.5 times your height."
+        ]
+    },
+    {
+        title: "Improvise",
+        icon: "skateboarding-01",
+        subtitle: "Any stunt not on this list.",
+        description: "Perform any kind of movement you can imagine.",
+        reference: "???",
+        bullets: [
+            "When you describe a kind of movement not detailed elsewhere in the rules, the DM tells you whether it is possible and what kind of roll you need to make, if any, to determine success or failure."
+        ]
+    },
+    {
+        title: "Long Jump",
+        icon: "jump-across",
+        subtitle: "1 foot moved costs 1 foot.",
+        description: "Make a horizontal leap.",
+        reference: "???",
+        bullets: [
+            "When you make a Long Jump, you leap horizontally a number of feet up to your Strength score if you move at least 10 feet immediately before the jump. When you make a standing Long Jump, you can leap only half that distance. Either way, each foot you jump costs a foot of movement.",
+            "If you land in Difficult Terrain, you must succeed on a DC 10 Dexterity (Acrobatics) check or have the Prone condition.",
+            "This Long Jump rule assumes that the height of the jump doesn't matter, such as a jump across a stream or a chasm. At your DM's discretion, you must succeed on a DC 10 Strength (Athletics) check to clear a low obstacle (no taller than a quarter of the jump's distance), such as a hedge or a low wall. Otherwise, you hit the obstacle."
+        ]
+    },
+    {
+        title: "Move",
+        icon: "run",
+        subtitle: "1 foot moved costs 1 foot.",
+        description: "Move around normally.",
+        reference: "???",
+        bullets: [
+            "On your turn, you can move a distance equal to your Speed or less. Or you can decide not to move.",
+            "Your movement can include climbing, crawling, jumping, and swimming. These different modes of movement can be combined with your regular movement, or they can constitute your entire move.",
+            "However you’re moving with your Speed, you deduct the distance of each part of your move from it until it is used up or until you are done moving, whichever comes first.",
+            "<b><i>Moving around Other Creatures.</i></b> During your move, you can pass through the space of an ally, a creature that has the Incapacitated condition, a Tiny creature, or a creature that is two sizes larger or smaller than you. Another creature’s space is Difficult Terrain for you unless that creature is Tiny or your ally. You can’t willingly end a move in a space occupied by another creature. If you somehow end a turn in a space with another creature, you have the Prone condition unless you are Tiny or are of a larger size than the other creature."
+        ]
+    },
+    {
+        title: "Stand Up",
+        icon: "strong",
+        subtitle: "Costs half your movement speed.",
+        description: "Stand up from Prone.",
+        reference: "See Prone [Condition]",
+        bullets: [
+            "To stand up, spend an amount of movement equal to half your Speed (round down) to right yourself and thereby end the Prone condition. If your Speed is 0, you can’t right yourself."
+        ]
+    },
+    {
+        title: "Swim",
+        icon: "at-sea",
+        subtitle: "1 foot moved costs 1 extra foot.",
+        description: "Swim through water.",
+        reference: "???",
+        bullets: [
+            "While you’re swimming, each foot of movement costs 1 extra foot (2 extra feet in Difficult Terrain). You ignore this extra cost if you have a Swim Speed and use it to swim.",
+            "At the DM’s discretion, moving any distance in rough water might require a successful DC 15 Strength (Athletics) check."
         ]
     }
 ]
