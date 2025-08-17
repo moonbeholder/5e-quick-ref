8e7b9c58544a95301251c8e3a70c3a4458751df1
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-18 00:10:04 +0900

    Update data_condition.js

--- js/data_condition.js
+++ js/data_condition.js
@@ -3,7 +3,7 @@ data_condition = [
         title: "Blinded",
         icon: "blindfold",
         subtitle: "You can't see.",
-        description: "You can't see, and fighting becomes difficult.",
+        description: "You are plunged into darkness, unable to see.",
         reference: "See Rules Glossary: Blinded [Condition]",
         bullets: [
             "While you have the Blinded condition, you experience the following effects.",
@@ -14,8 +14,8 @@ data_condition = [
     {
         title: "Charmed",
         icon: "smitten",
-        subtitle: "You are charmed",
-        description: "You are charmed by another creature",
+        subtitle: "You are charmed.",
+        description: "You are wrapped in a haze of affection, admiration, or trust toward another creature.",
         reference: "See Rules Glossary: Charmed [Condition]",
         bullets: [
             "While you have the Charmed condition, you experience the following effects.",
@@ -27,7 +27,7 @@ data_condition = [
         title: "Deafened",
         icon: "hearing-disabled",
         subtitle: "You can't hear.",
-        description: "You can't hear.",
+        description: "You are plunged into sudden silence.",
         reference: "See Rules Glossary: Deafened [Condition]",
         bullets: [
             "While you have the Deafened condition, you experience the following effect.",
@@ -36,9 +36,9 @@ data_condition = [
     },
     {
         title: "Exhaustion",
-        icon: "crawl",
-        subtitle: "You are exhausted",
-        description: "Exhaustion is measured in six levels",
+        icon: "despair",
+        subtitle: "You are exhausted.",
+        description: "You are worn out physically, mentally, or both.",
         reference: "See Rules Glossary: Exhaustion [Condition]",
         bullets: [
             "While you have the Exhaustion condition, you experience the following effects.",
@@ -51,153 +51,169 @@ data_condition = [
     {
         title: "Frightened",
         icon: "sharp-smile",
-        subtitle: "You are frightened",
-        description: "You are frightened",
-        reference: "PHB, pg. 290.",
+        subtitle: "You are frightened.",
+        description: "You are overwhelmed by fear.",
+        reference: "See Rules Glossary: Frightened [Condition]",
         bullets: [
-            "You have disadvantage on ability checks and attack rolls while the source of your fear is within line of sight.",
-            "You can't willingly move closer to the source of your fear."
+            "While you have the Frightened condition, you experience the following effects.",
+            "<b>Ability Checks and Attacks Affected.</b> You have Disadvantage on ability checks and attack rolls while the source of fear is within line of sight.",
+            "<b>Can’t Approach.</b> You can’t willingly move closer to the source of fear."
         ]
     },
     {
         title: "Grappled",
         icon: "grab",
-        subtitle: "You are grappled",
-        description: "You are grappled",
-        reference: "PHB, pg. 290.",
+        subtitle: "You are grappled.",
+        description: "You are held in place by another creature, unable to move freely.",
+        reference: "See Rules Glossary: Grappled [Condition]",
         bullets: [
-            "Your speed becomes 0, and you can't benefit from any bonus to your speed.",
-            "The condition ends if your grappler is incapacitated.",
-            "The condition also ends if you are removed from the reach of your grappler."
+            "While you have the Grappled condition, you experience the following effects.",
+            "<b>Speed 0.</b> Your Speed is 0 and can’t increase.",
+            "<b>Attacks Affected.</b> You have Disadvantage on attack rolls against any target other than the grappler.",
+            "<b>Movable.</b> The grappler can drag or carry you when it moves, but every foot of movement costs it 1 extra foot unless you are Tiny or two or more sizes smaller than it.",
+            "<b>Ending a Grapple.</b> A Grappled creature can use its action to make a Strength (Athletics) or Dexterity (Acrobatics) check against the grapple’s escape DC, ending the condition on itself on a success. The condition also ends if the grappler has the Incapacitated condition or if the distance between the Grappled target and the grappler exceeds the grapple’s range. In addition, the grappler can release the target at any time (no action required)."
         ]
     },
     {
         title: "Incapacitated",
         icon: "internal-injury",
-        subtitle: "You can't take actions or reactions",
-        description: "You can't take actions or reactions",
-        reference: "PHB, pg. 290.",
+        subtitle: "No actions, bonus actions, or reactions.",
+        description: "You are unable to act, as your mind or body temporarily shuts down.",
+        reference: "See Rules Glossary: Incapacitated [Condition]",
         bullets: [
+            "While you have the Incapacitated condition, you experience the following effects.",
+            "<b>Inactive.</b> You can’t take any action, Bonus Action, or Reaction.",
+            "<b>No Concentration.</b> Your Concentration is broken.",
+            "<b>Speechless.</b> You can’t speak.",
+            "<b>Surprised.</b> If you’re Incapacitated when you roll Initiative, you have Disadvantage on the roll."
         ]
     },
     {
         title: "Invisible",
         icon: "invisible",
-        subtitle: "You can't be seen",
-        description: "You can't be seen without the aid of magic or a special sense",
-        reference: "PHB, pg. 291.",
+        subtitle: "You can't be seen.",
+        description: "You are hidden from sight as though vanished from the world.",
+        reference: "See Rules Glossary: Invisible [Condition]",
         bullets: [
-            "For the purpose of hiding, you are heavily obscured.",
-            "You can still be detected by any noise you make or tracks you leave.",
-            "You have advantage on attack rolls.",
-            "Attack rolls against you have disadvantage."
+            "While you have the Invisible condition, you experience the following effects.",
+            "<b>Surprise.</b> If you’re Invisible when you roll Initiative, you have Advantage on the roll.",
+            "<b>Concealed.</b> You aren’t affected by any effect that requires its target to be seen unless the effect’s creator can somehow see you. Any equipment you are wearing or carrying is also concealed.",
+            "<b>Attacks Affected.</b> Attack rolls against you have Disadvantage, and your attack rolls have Advantage. If a creature can somehow see you, you don’t gain this benefit against that creature."
         ]
     },
     {
         title: "Paralyzed",
         icon: "internal-injury",
-        subtitle: "You are paralyzed",
-        description: "You can't do anything",
+        subtitle: "You are paralyzed.",
+        description: "You are frozen in place, unable to move or act, your body locked rigid.",
+        reference: "See Rules Glossary: Paralyzed [Condition]",
         bullets: [
-            "You are incapacitated and can't move or speak.",
-            "Attack rolls against you have advantage.",
-            "Any attack that hits you is a critical hit if the attacker is within 5 feet of you.",
-            "You automatically fail Strength and Dexterity saving throws."
+            "While you have the Paralyzed condition, you experience the following effects.",
+            "<b>Incapacitated.</b> You have the Incapacitated condition.",
+            "<b>Speed 0.</b> Your Speed is 0 and can’t increase.",
+            "<b>Saving Throws Affected.</b> You automatically fail Strength and Dexterity saving throws.",
+            "<b>Attacks Affected.</b> Attack rolls against you have Advantage.",
+            "<b>Automatic Critical Hits.</b> Any attack roll that hits you is a Critical Hit if the attacker is within 5 feet of you."
         ]
     },
     {
         title: "Petrified",
-        icon: "stone-pile",
-        subtitle: "You are transformed into stone",
-        description: "You are transformed, along with any nonmagical objects you are wearing or carrying, into a solid inanimate substance (usually stone)",
-        reference: "PHB, pg. 291.",
+        icon: "rock",
+        subtitle: "You are turned to stone.",
+        description: "You are transformed into solid stone, frozen in place and unaware of the world around you.",
+        reference: "See Rules Glossary: Petrified [Condition]",
         bullets: [
-            "Your weight increases by a factor of ten, and you cease aging.",
-            "You are incapacitated, can't move or speak, and are unaware of your surroundings.",
-            "Attack rolls against you have advantage.",
-            "You automatically fail Strength and Dexterity saving throws.",
-            "You have resistance to all damage.",
-            "You are immune to poison and disease, though a poison or disease already in your system is only suspended, not neutralized."
+            "While you have the Petrified condition, you experience the following effects.",
+            "<b>Turned to Inanimate Substance.</b> You are transformed, along with any nonmagical objects you are wearing and carrying, into a solid inanimate substance (usually stone). Your weight increases by a factor of ten, and you cease aging.",
+            "<b>Incapacitated.</b> You have the Incapacitated condition.",
+            "<b>Speed 0.</b> Your Speed is 0 and can’t increase.",
+            "<b>Attacks Affected.</b> Attack rolls against you have Advantage.",
+            "<b>Saving Throws Affected.</b> You automatically fail Strength and Dexterity saving throws.",
+            "<b>Resist Damage.</b> You have Resistance to all damage.",
+            "<b>Poison Immunity.</b> You have Immunity to the Poisoned condition."
         ]
     },
     {
         title: "Poisoned",
-        icon: "deathcab",
-        subtitle: "You are poisoned",
-        description: "You are poisoned",
-        reference: "PHB, pg. 292.",
+        icon: "poison-bottle",
+        subtitle: "You are poisoned.",
+        description: "Your body is weakened by toxins, causing nausea and unsteadiness.",
+        reference: "See Rules Glossary: Poisoned [Condition]",
         bullets: [
-            "You have disadvantage on attack rolls and ability checks."
+            "While you have the Poisoned condition, you experience the following effect.",
+            "<b>Ability Checks and Attacks Affected.</b> You have Disadvantage on attack rolls and ability checks."
         ]
     },
     {
         title: "Prone",
         icon: "crawl",
-        subtitle: "You are prone",
-        description: "You are prone",
-        reference: "PHB, pg. 292.",
+        subtitle: "You are prone.",
+        description: "You are sprawled on the ground and vulnerable.",
+        reference: "See Rules Glossary: Prone [Condition]",
         bullets: [
-            "Your only movement option is to crawl, unless you stand up.",
-            "You have disadvantage on attack rolls.",
-            "Attack rolls against you have advantage if the attacker is within 5 feet of you, otherwise the attack roll has disadvantage."
+            "While you have the Prone condition, you experience the following effects.",
+            "<b>Restricted Movement.</b> Your only movement options are to crawl or to spend an amount of movement equal to half your Speed (round down) to right yourself and thereby end the condition. If your Speed is 0, you can’t right yourself.",
+            "<b>Attacks Affected.</b> You have Disadvantage on attack rolls. An attack roll against you has Advantage if the attacker is within 5 feet of you. Otherwise, that attack roll has Disadvantage."
         ]
     },
     {
         title: "Restrained",
         icon: "imprisoned",
-        subtitle: "You are restrained",
-        description: "You are restrained",
-        reference: "PHB, pg. 292.",
+        subtitle: "You are restrained.",
+        description: "You are bound or trapped, unable to move freely.",
+        reference: "See Rules Glossary: Restrained [Condition]",
         bullets: [
-            "Your speed becomes 0, and you can't benefit from any bonus to your speed.",
-            "You have disadvantage on attack rolls.",
-            "Attack rolls against you have advantage.",
-            "You have disadvantage on Dexterity saving throws."
+            "While you have the Restrained condition, you experience the following effects.",
+            "<b>Speed 0.</b> Your Speed is 0 and can’t increase.",
+            "<b>Attacks Affected.</b> Attack rolls against you have Advantage, and your attack rolls have Disadvantage.",
+            "<b>Saving Throws Affected.</b> You have Disadvantage on Dexterity saving throws."
         ]
     },
     {
         title: "Stunned",
-        icon: "internal-injury",
-        subtitle: "You are stunned",
-        description: "You are stunned",
-        reference: "PHB, pg. 292.",
+        icon: "knocked-out-stars",
+        subtitle: "You are stunned.",
+        description: "You are dazed and overwhelmed, unable to act with clarity.",
+        reference: "See Rules Glossary: Stunned [Condition]",
         bullets: [
-            "You are incapacitated, can't move, and can speak only falteringly.",
-            "Attack rolls against you have advantage.",
-            "You automatically fail Strength and Dexterity saving throws."
+            "While you have the Stunned condition, you experience the following effects.",
+            "<b>Incapacitated.</b> You have the Incapacitated condition.",
+            "<b>Saving Throws Affected.</b> You automatically fail Strength and Dexterity saving throws.",
+            "<b>Attacks Affected.</b> Attack rolls against you have Advantage."
         ]
     },
     {
         title: "Unconscious",
         icon: "coma",
-        subtitle: "You are unconscious",
-        description: "You are unconscious",
-        reference: "PHB, pg. 292.",
+        subtitle: "You are unconscious.",
+        description: "",
+        reference: "See Rules Glossary: Unconscious [Condition]",
         bullets: [
-            "You are incapacitated, can't move or speak, and are unaware of your surroundings.",
-            "You drop whatever you're holding and fall prone.",
-            "Attack rolls against you have advantage.",
-            "Any attack that hits you is a critical hit if the attacker is within 5 feet of you.",
-            "You automatically fail Strength and Dexterity saving throws.",
+            "While you have the Unconscious condition, you experience the following effects.",
+            "<b>Inert.</b> You have the Incapacitated and Prone conditions, and you drop whatever you’re holding. When this condition ends, you remain Prone.",
+            "<b>Speed 0.</b> Your Speed is 0 and can’t increase.",
+            "<b>Attacks Affected.</b> Attack rolls against you have Advantage.",
+            "<b>Saving Throws Affected.</b> You automatically fail Strength and Dexterity saving throws.",
+            "<b>Automatic Critical Hits.</b> Any attack roll that hits you is a Critical Hit if the attacker is within 5 feet of you.",
+            "<b>Unaware.</b> You’re unaware of your surroundings."
         ]
     },
     {
         title: "Dying",
-        icon: "dead-head",
-        subtitle: "You are dying",
-        description: "You have been dropped to zero hit points and are dying",
-        reference: "PHB, pg. 197.",
+        icon: "backstab",
+        subtitle: "You are dying.",
+        description: "You hover on the brink of death, barely clinging to life.",
+        reference: "See Playing the Game: Damage and Healing (Dropping to Zero Hit Points)",
         bullets: [
-            "If you are reduced to 0 hit points by damage that fails to kill you, you fall unconscious and are dying.",
-            "If you receive any healing you immediately regain consciousness again and no longer dying.",
-            "When dying, at the start of each of your turns you make a death saving throw. Roll a d20 and do not add any modifiers.",
-            "A 10 or higher is a success, 9 or lower is a failure.",
-            "On your third success, you become stable.",
-            "On your third failure, you die.",
-            "Rolling a 1 counts as two failures.",
-            "Rolling a 20 immediately causes you to regain 1 hit point.",
-            "You can also be stabilized by an ally taking the Stabilize action and succeeding on a DC 10 Wisdom (Medicine) check.",
-            "Once stable, you regain 1 hit point after 1d4 hours."
+            "When a creature drops to 0 Hit Points, it either dies outright or falls unconscious.",
+            "<b>Hit Point Maximum of 0.</b> A creature dies if its Hit Point maximum reaches 0. Certain effects drain life energy, reducing a creature’s Hit Point maximum.",
+            "<b>Massive Damage.</b> When damage reduces a character to 0 Hit Points and damage remains, the character dies if the remainder equals or exceeds their Hit Point maximum. For example, if your character has a Hit Point maximum of 12, currently has 6 Hit Points, and takes 18 damage, the character drops to 0 Hit Points, but 12 damage remains. The character then dies, since 12 equals their Hit Point maximum.",
+            "<b>Falling Unconscious</b> If you reach 0 Hit Points and don’t die instantly, you have the Unconscious condition until you regain any Hit Points, and you now face making Death Saving Throws.",
+            "<b>Death Saving Throws.</b> Whenever you start your turn with 0 Hit Points, you must make a Death Saving Throw to determine whether you creep closer to death or hang on to life. Unlike other saving throws, this one isn’t tied to an ability score. You’re in the hands of fate now.",
+            "<b>Three Successes/Failures.</b> Roll 1d20. If the roll is 10 or higher, you succeed. Otherwise, you fail. A success or failure has no effect by itself. On your third success, you become Stable. On your third failure, you die. The successes and failures don’t need to be consecutive; keep track of both until you collect three of a kind. The number of both is reset to zero when you regain any Hit Points or become Stable.",
+            "<b>Rolling a 1 or 20.</b> When you roll a 1 on the d20 for a Death Saving Throw, you suffer two failures. If you roll a 20 on the d20, you regain 1 Hit Point.",
+            "<b>Damage at 0 Hit Points.</b> If you take any damage while you have 0 Hit Points, you suffer a Death Saving Throw failure. If the damage is from a Critical Hit, you suffer two failures instead. If the damage equals or exceeds your Hit Point maximum, you die.",
+            "<b>Stabilizing a Character.</b> You can take the Help action to try to stabilize a creature with 0 Hit Points, which requires a successful DC 10 Wisdom (Medicine) check. A Stable creature doesn’t make Death Saving Throws even though it has 0 Hit Points, but it still has the Unconscious condition. If the creature takes damage, it stops being Stable and starts making Death Saving Throws again. A Stable creature that isn’t healed regains 1 Hit Point after 1d4 hours."
         ]
     }
 ]
