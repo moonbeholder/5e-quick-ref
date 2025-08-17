b3b7be9d693e03a7adfa4295f1f2d4c6ff25c3fd
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-17 16:52:43 +0900

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -3,8 +3,8 @@ data_bonusaction = [
         title: "Extra Attack",
         icon: "sword-brandish",
         subtitle: "Make an extra attack.",
-        description: "Attack with another Light weapon.",
-        reference: "???",
+        description: "Make an extra attack with a Light weapon held in a different hand.",
+        reference: "See Equipment: Weapons (Properties: Light)",
         bullets: [
             "When you take the Attack action on your turn and attack with a Light weapon, you can make one extra attack as a Bonus Action later on the same turn. That extra attack must be made with a different Light weapon, and you don’t add your ability modifier to the extra attack’s damage unless that modifier is negative. For example, you can attack with a Shortsword in one hand and a Dagger in the other using the Attack action and a Bonus Action, but you don’t add your Strength or Dexterity modifier to the damage roll of the Bonus Action unless that modifier is negative."
         ]
@@ -14,9 +14,11 @@ data_bonusaction = [
         icon: "magic-swirl",
         subtitle: "Cast time of 1 bonus action.",
         description: "Cast a spell as a bonus action",
-        reference: "???",
+        reference: "See Spells: Casting Spells (Casting Time)",
         bullets: [
-            "You cast a spell that has a casting time of 1 Bonus Action."
+            "Most spells require the Magic action to cast, but some spells require a Bonus Action. A spell’s Casting Time entry specifies whether it takes an Action, a Bonus Action, a Reation, etc.",
+            "<b>Bonus Action Triggers.</b> A spell that has a casting time of a Reaction is cast in response to a trigger that is defined in the spell’s Casting Time entry. Some spells that have a casting time of a Bonus Action are also cast in response to a trigger defined in the spell.You cast a spell that has a casting time of 1 Bonus Action.",
+            "<b>One Spell with a Spell Slot per Turn.</b> On a turn, you can expend only one spell slot to cast a spell. This rule means you can’t, for example, cast a spell with a spell slot using the Magic action and another one using a Bonus Action on the same turn."
         ]
     },
     {
@@ -24,7 +26,7 @@ data_bonusaction = [
         icon: "embraced-energy",
         subtitle: "Some features use bonus actions",
         description: "Use a racial or class feature that uses a bonus action",
-        reference: "See  page for more information.",
+        reference: "See class or ability page for more information.",
         bullets: [
             "You can take a Bonus Action if a special ability, or another feature of the game states that you can do something as a Bonus Action."
         ]
