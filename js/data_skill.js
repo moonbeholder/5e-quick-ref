73747255c48e7abf611baa242bb02eb97137c81f
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-21 22:23:48 +0900

    Update data_skill.js

--- js/data_skill.js
+++ js/data_skill.js
@@ -1,10 +1,182 @@
 data_skill = [
     {
-        title: "",
+        title: "Acrobatics",
+        icon: "acrobatic",
+        subtitle: "Dexterity check",
+        description: "Stay on your feet in a tricky situation, or perform an acrobatic stunt.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Animal Handling",
+        icon: "sitting-dog",
+        subtitle: "Wisdom check",
+        description: "Calm or train an animal, or get an animal to behave in a certain way.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Arcana",
+        icon: "secret-book",
+        subtitle: "Intelligence check",
+        description: "Recall lore about spells, magic items, and the planes of existence.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Athletics",
+        icon: "biceps",
+        subtitle: "Strength check",
+        description: "Jump farther than normal, stay afloat in rough water, or break something.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "Strength (Athletics) check covers difficult situations you encounter while climbing, jumping, or swimming.",
+            "Examples include the following activities:</br><ul><li>You attempt to climb a sheer or slippery cliff, avoid hazards while scaling a wall, or cling to a surface while something is trying to knock you off.</li><li>You try to jump an unusually long distance or pull off a stunt midjump.</li><li>You struggle to swim or stay afloat in treacherous currents, storm-tossed waves, or areas of thick seaweed. Or another creature tries to push or pull you underwater or otherwise interfere with your swimming.</li></ul>",
+            "The DM might also call for a Strength check when you try to accomplish tasks like the following:</br><ul><li>Force open a stuck, locked, or barred door</li><li>Break free of bonds</li><li>Push through a tunnel that is too small</li><li>Hang on to a wagon while being dragged behind it</li><li>Tip over a statue</li><li>Keep a boulder from rolling</li></ul>"
+        ]
+    },
+    {
+        title: "Deception",
+        icon: "fingers-crossed",
+        subtitle: "Charisma check",
+        description: "Tell a convincing lie, or wear a disguise convincingly.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "History",
+        icon: "archive-research",
+        subtitle: "Intelligence check",
+        description: "Recall lore about historical events, people, nations, and cultures.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Insight",
+        icon: "think",
+        subtitle: "WIS check",
+        description: "Discern a person’s mood and intentions.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Intimidation",
+        icon: "angry-eyes",
+        subtitle: "Charisma check",
+        description: "Awe or threaten someone into doing what you want.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Investigation",
+        icon: "sherlock-holmes",
+        subtitle: "Intelligence check",
+        description: "Find obscure information in books, or deduce how something works.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Medicine",
+        icon: "first-aid-kit",
+        subtitle: "Wisdom check",
+        description: "Diagnose an illness, or determine what killed the recently slain.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Nature",
+        icon: "oak-leaf",
+        subtitle: "Intelligence check",
+        description: "Recall lore about terrain, plants, animals, and weather.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Perception",
         icon: "",
-        subtitle: "",
-        description: "",
+        subtitle: "Wisdom check",
+        description: "Using a combination of senses, notice something that’s easy to miss.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Performance",
+        icon: "drama-masks",
+        subtitle: "Charisma check",
+        description: "Act, tell a story, perform music, or dance.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Persuasion",
+        icon: "convince",
+        subtitle: "Charisma check",
+        description: "Honestly and graciously convince someone of something.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Religion",
+        icon: "prayer",
+        subtitle: "Intelligence check",
+        description: "Recall lore about gods, religious rituals, and holy symbols.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Sleight of Hand",
+        icon: "magic-trick",
+        subtitle: "Dexterity check",
+        description: "Pick a pocket, conceal a handheld object, or perform legerdemain.",
         reference: "",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Stealth",
+        icon: "hood",
+        subtitle: "Dexterity check",
+        description: "Escape notice by moving quietly and hiding behind things.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Survival",
+        icon: "hiking",
+        subtitle: "Wisdom check",
+        description: "Follow tracks, forage, find a trail, or avoid natural hazards.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
         bullets: [
             ""
         ]
