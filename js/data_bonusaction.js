541745da2c755e1a59856421d584634adf1af365
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-21 14:50:35 +0900

    Update data_bonusaction.js

--- js/data_bonusaction.js
+++ js/data_bonusaction.js
@@ -2,8 +2,8 @@ data_bonusaction = [
     {
         title: "★ Extra Attack",
         icon: "sword-brandish",
-        subtitle: "Make an extra attack.",
-        description: "Make an extra attack with a Light weapon held in a different hand.",
+        subtitle: "Make an off-hand attack.",
+        description: "Make an additional attack with a light weapon in the off-hand.",
         reference: "See Equipment: Weapons (Properties: Light)",
         bullets: [
             "When you take the Attack action on your turn and attack with a Light weapon, you can make one extra attack as a Bonus Action later on the same turn. That extra attack must be made with a different Light weapon, and you don’t add your ability modifier to the extra attack’s damage unless that modifier is negative. For example, you can attack with a Shortsword in one hand and a Dagger in the other using the Attack action and a Bonus Action, but you don’t add your Strength or Dexterity modifier to the damage roll of the Bonus Action unless that modifier is negative."
@@ -12,8 +12,8 @@ data_bonusaction = [
     {
         title: "Cast a Spell",
         icon: "magic-swirl",
-        subtitle: "Cast time of 1 bonus action.",
-        description: "Cast a spell as a bonus action.",
+        subtitle: "Cast a quick spell",
+        description: "Cast a spell that requires a bonus action.",
         reference: "See Spells: Casting Spells (Casting Time)",
         bullets: [
             "Most spells require the Magic action to cast, but some spells require a Bonus Action. A spell’s Casting Time entry specifies whether it takes an Action, a Bonus Action, a Reation, etc.",
@@ -24,8 +24,8 @@ data_bonusaction = [
     {
         title: "Other Ability",
         icon: "embraced-energy",
-        subtitle: "Some features use bonus actions.",
-        description: "Use a racial or class feature that uses a bonus action.",
+        subtitle: "Use a character ability",
+        description: "Perform a bonus action tied to an ability or trait.",
         reference: "See class or ability page for more information.",
         bullets: [
             "You can take a Bonus Action if a special ability, or another feature of the game states that you can do something as a Bonus Action."
