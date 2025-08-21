b8b678e0d08004d90e1e526ddff79ec4c51f5652
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-21 19:52:45 +0900

    Update and rename data_skills.js to data_skill.js

---
+++ js/data_skill.js
@@ -0,0 +1,12 @@
+data_skill = [
+    {
+        title: "",
+        icon: "",
+        subtitle: "",
+        description: "",
+        reference: "",
+        bullets: [
+            ""
+        ]
+    }
+]
