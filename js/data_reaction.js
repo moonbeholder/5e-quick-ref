891038fb279786360b0c9a0276a307f1ca5765b7
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-21 15:54:42 +0900

    Update data_reaction.js

--- js/data_reaction.js
+++ js/data_reaction.js
@@ -1,9 +1,9 @@
 data_reaction = [
     {
-        title: "Opportunity Attack",
+        title: "Attack (Opportunity)",
         icon: "sword-brandish",
-        subtitle: "Attack a fleeing enemy.",
-        description: "Combatants watch for enemies to drop their guard. Moving heedlessly past foes, puts you in danger by provoking an Opportunity Attack.",
+        subtitle: "Attack a fleeing enemy",
+        description: "Make a melee attack against an enemy that moves out of reach.",
         reference: "See Playing the Game: Combat (Melee Attacks), Rules Glossary: Opportunity Attacks",
         bullets: [
             "You can make an Opportunity Attack when a creature that you can see leaves your reach using its action, its Bonus Action, its Reaction, or one of its speeds. To make the Opportunity Attack, take a Reaction to make one melee attack with a weapon or an Unarmed Strike against the provoking creature. The attack occurs right before the creature leaves your reach.",
@@ -12,24 +12,24 @@ data_reaction = [
         ]
     },
     {
+        title: "Cast a Spell",
+        icon: "magic-swirl",
+        subtitle: "Cast a reactive spell",
+        description: "Use a reaction to cast a spell when its trigger is met.",
+        reference: "See Spells: Casting Spells (Casting Time)",
+        bullets: [
+            "<b>Reaction Triggers.</b> A spell that has a casting time of a Reaction is cast in response to a trigger that is defined in the spell’s Casting Time entry."
+        ]
+    },
+    {
         title: "Readied Action",
         icon: "stopwatch",
-        subtitle: "Do the readied Action.",
-        description: "Execute the reaction specified by your Ready action, in response to the chosen trigger.",
+        subtitle: "Act on a trigger",
+        description: "Carry out a prepared action when the chosen condition occurs.",
         reference: "See Rules Glossary: Ready [Action]",
         bullets: [
             "When the trigger occurs, you can either take your Reaction right after the trigger finishes or ignore the trigger.",
             "<b>Readying a Spell.</b> When you Ready a spell, you cast it as normal (expending any resources used to cast it) but hold its energy, which you release with your Reaction when the trigger occurs. To be readied, a spell must have a casting time of an action, and holding on to the spell’s magic requires Concentration, which you can maintain up to the start of your next turn. If your Concentration is broken, the spell dissipates without taking effect."
-        ]
-    },
-    {
-        title: "Cast a Spell",
-        icon: "magic-swirl",
-        subtitle: "Cast a spell as a reaction.",
-        description: "Cast a spell with a casting time of 1 reaction.",
-        reference: "See Spells: Casting Spells (Casting Time)",
-        bullets: [
-            "<b>Reaction Triggers.</b> A spell that has a casting time of a Reaction is cast in response to a trigger that is defined in the spell’s Casting Time entry."
         ]
     }
 ]
