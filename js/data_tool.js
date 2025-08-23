1412b873f3d3499d20e731746f7ba58283073850
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-24 03:04:43 +0900

    Update data_tool.js

--- js/data_tool.js
+++ js/data_tool.js
@@ -1,84 +1,301 @@
 data_tool_artisan = [
     {
-        title: "",
+        title: "Alchemist's Supplies",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 50 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Identify a substance (DC 15), or start a fire (DC 15)",
+            "<b>Craft:</b> Acid, Alchemist’s Fire, Component Pouch, Oil, Paper, Perfume"
         ]
     },
     {
-        title: "",
+        title: "Brewer's Supplies",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 20 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Detect poisoned drink (DC 15), or identify alcohol (DC 10)",
+            "<b>Craft:</b> Antitoxin"
         ]
     },
     {
-        title: "",
+        title: "Calligrapher's Supplies",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 10 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Write text with impressive flourishes that guard against forgery (DC 15)",
+            "<b>Craft:</b> Ink, Spell Scroll"
         ]
     },
     {
-        title: "",
+        title: "Carpenter's Tools",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 8 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Strength",
+            "<b>Utilize:</b> Seal or pry open a door or container (DC 20)",
+            "<b>Craft:</b> Club, Greatclub, Quarterstaff, Barrel, Chest, Ladder, Pole, Portable Ram, Torch"
+        ]
+    },
+    {
+        title: "Cartographer's Tools",
+        icon: "",
+        subtitle: "Cost: 15 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Wisdom",
+            "<b>Utilize:</b> Draft a map of a small area (DC 15)",
+            "<b>Craft:</b> Map"
+        ]
+    },
+    {
+        title: "Cobbler's Tools",
+        icon: "",
+        subtitle: "Cost: 5 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Modify footwear to give Advantage on the wearer’s next Dexterity (Acrobatics) check (DC 10)",
+            "<b>Craft:</b> Climber’s Kit"
+        ]
+    },
+    {
+        title: "Cook's Utensils",
+        icon: "",
+        subtitle: "Cost: 1 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Wisdom",
+            "<b>Utilize:</b> Improve food’s flavor (DC 10), or detect spoiled or poisoned food (DC 15)",
+            "<b>Craft:</b> Rations"
+        ]
+    },
+    {
+        title: "Glassblower's Tools",
+        icon: "",
+        subtitle: "Cost: 30 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Discern what a glass object held in the past 24 hours (DC 15)",
+            "<b>Craft:</b> Glass Bottle, Magnifying Glass, Spyglass, Vial"
+        ]
+    },
+    {
+        title: "Jeweler's Tools",
+        icon: "",
+        subtitle: "Cost: 25 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Discern a gem’s value (DC 15)",
+            "<b>Craft:</b> Arcane Focus, Holy Symbol"
+        ]
+    },
+    {
+        title: "Leatherworker's Tools",
+        icon: "",
+        subtitle: "Cost: 5 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Add a design to a leather item (DC 10)",
+            "<b>Craft:</b> Sling, Whip, Hide Armor, Leather Armor, Studded Leather Armor, Backpack, Crossbow Bolt Case, Map or Scroll Case, Parchment, Pouch, Quiver, Waterskin"
+        ]
+    },
+    {
+        title: "Mason's Tools",
+        icon: "",
+        subtitle: "Cost: 10 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Strength",
+            "<b>Utilize:</b> Chisel a symbol or hole in stone (DC 10)",
+            "<b>Craft:</b> Block and Tackle"
+        ]
+    },
+    {
+        title: "Painter's Supplies",
+        icon: "",
+        subtitle: "Cost: 10 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Wisdom",
+            "<b>Utilize:</b> Paint a recognizable image of something you’ve seen (DC 10)",
+            "<b>Craft:</b> Druidic Focus, Holy Symbol"
+        ]
+    },
+    {
+        title: "Potter's Tools",
+        icon: "",
+        subtitle: "Cost: 10 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Discern what a ceramic object held in the past 24 hours (DC 15)",
+            "<b>Craft:</b> Jug, Lamp"
+        ]
+    },
+    {
+        title: "Smith's Tools",
+        icon: "",
+        subtitle: "Cost: 20 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Strength",
+            "<b>Utilize:</b> Pry open a door or container (DC 20)",
+            "<b>Craft:</b> Any Melee weapon (except Club, Greatclub, Quarterstaff, and Whip), Medium armor (except Hide), Heavy armor, Ball Bearings, Bucket, Caltrops, Chain, Crowbar, Firearm Bullets, Grappling Hook, Iron Pot, Iron Spikes, Sling Bullets"
+        ]
+    },
+    {
+        title: "Tinker's Tools",
+        icon: "",
+        subtitle: "Cost: 50 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Assemble a Tiny item composed of scrap, which falls apart in 1 minute (DC 20)",
+            "<b>Craft:</b> Musket, Pistol, Bell, Bullseye Lantern, Flask, Hooded Lantern, Hunter’s Trap, Lock, Manacles, Mirror, Shovel, Signal Whistle, Tinderbox"
+        ]
+    },
+    {
+        title: "Weaver's Tools",
+        icon: "",
+        subtitle: "Cost: 1 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Mend a tear in clothing (DC 10), or sew a Tiny design (DC 10)",
+            "<b>Craft:</b> Padded Armor, Basket, Bedroll, Blanket, Fine Clothes, Net, Robe, Rope, Sack, String, Tent, Traveler's Clothes"
+        ]
+    },
+    {
+        title: "Woodcarver's Tools",
+        icon: "",
+        subtitle: "Cost: 1 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Carve a pattern in wood (DC 10)",
+            "<b>Craft:</b> Club, Greatclub, Quarterstaff, Ranged weapons (except Pistol, Musket, and Sling), Arcane Focus, Arrows, Bolts, Druidic Focus, Ink Pen, Needles"
         ]
     }
 ]
 data_tool_other = [
     {
-        title: "",
+        title: "Disguise Kit",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 25 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Charisma",
+            "<b>Utilize:</b> Apply makeup (DC 10)",
+            "<b>Craft:</b> Costume"
         ]
     },
     {
-        title: "",
+        title: "Forgery Kit",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 15 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Mimic 10 or fewer words of someone else’s handwriting (DC 15), or duplicate a wax seal (DC 20)"
         ]
     },
     {
-        title: "",
+        title: "Gaming Set",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: varies",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Wisdom",
+            "<b>Utilize:</b> Discern whether someone is cheating (DC 10), or win the game (DC 20)",
+            "<b>Variants:</b> Dice (1 sp), dragonchess (1 gp), playing cards (5 sp), three-dragon ante (1 gp)"
         ]
     },
     {
-        title: "",
+        title: "Herbalism Kit",
         icon: "",
-        subtitle: "",
+        subtitle: "Cost: 5 gp",
         description: "",
-        reference: "",
+        reference: "See Equipment: Tools",
         bullets: [
-            ""
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Identify a plant (DC 10)",
+            "<b>Craft:</b> Antitoxin, Candle, Healer’s Kit, Potion of Healing"
+        ]
+    },
+    {
+        title: "Musical Instrument",
+        icon: "",
+        subtitle: "Cost: varies",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Charisma",
+            "<b>Utilize:</b> Play a known tune (DC 10), or improvise a song (DC 15)",
+            "<b>Variants:</b> Bagpipes (30 GP, 6 lb.), drum (6 GP, 3 lb.), dulcimer (25 GP, 10 lb.), flute (2 GP, 1 lb.), horn (3 GP, 2 lb.), lute (35 GP, 2 lb.), lyre (30 GP, 2 lb.), pan flute (12 GP, 2 lb.), shawm (2 GP, 1 lb.), viol (30 GP, 1 lb.)"
+        ]
+    },
+    {
+        title: "Navigator's Tools",
+        icon: "",
+        subtitle: "Cost: 25 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Wisdom",
+            "<b>Utilize:</b> Plot a course (DC 10), or determine position by stargazing (DC 15)"
+        ]
+    },
+    {
+        title: "Poisoner's Kit",
+        icon: "",
+        subtitle: "Cost: 50 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Intelligence",
+            "<b>Utilize:</b> Detect a poisoned object (DC 10)",
+            "<b>Craft:</b> Basic Poison"
+        ]
+    },
+    {
+        title: "Thieves' Tools",
+        icon: "",
+        subtitle: "Cost: 25 gp",
+        description: "",
+        reference: "See Equipment: Tools",
+        bullets: [
+            "<b>Ability:</b> Dexterity",
+            "<b>Utilize:</b> Pick a lock (DC 15), or disarm a trap (DC 15)"
         ]
     }
 ]
