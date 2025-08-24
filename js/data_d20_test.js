181f42dd446f555f7f2b53804565ab315fdd37c5
Michael Kemp <michaelkemp.coding@gmail.com>
2025-08-24 17:46:28 +0900

    Update and rename data_skill.js to data_d20_test.js

---
+++ js/data_d20_test.js
@@ -0,0 +1,256 @@
+data_d20_test_ability = [
+    {
+        title: "Strength",
+        icon: "",
+        subtitle: "Physical might",
+        description: "Strength measures bodily power, athletic training, and the extent to which you can exert raw physical force.",
+        reference: "See Playing the Game: D20 Tests",
+        bullets: [
+            "A Strength check can model any attempt to lift, push, pull, or break something, to force your body through a space, or to otherwise apply brute force to a situation.",
+            "The DM might call for a Strength check when you try to accomplish tasks like the following:</br><ul><li>Force open a stuck, locked, or barred door</li><li>Break free of bonds</li><li>Push through a tunnel that is too small</li><li>Hang on to a wagon while being dragged behind it</li><li>Tip over a statue</li><li>Keep a boulder from rolling</li></ul>"
+        ]
+    },
+    {
+        title: "Dexterity",
+        icon: "",
+        subtitle: "Agility, reflexes, & balance",
+        description: "Dexterity measures agility, reflexes, and balance.",
+        reference: "See Playing the Game: D20 Tests",
+        bullets: [
+            "A Dexterity check can model any attempt to move nimbly, quickly, or quietly, or to keep from falling on tricky footing.",
+            "The DM might call for a Dexterity check when you try to accomplish tasks like the following:</br><ul><li>Control a heavily laden cart on a steep descent</li><li>Steer a chariot around a tight turn</li><li>Securely tie up a prisoner</li><li>Wriggle free of bonds</li></ul>"
+        ]
+    },
+    {
+        title: "Constitution",
+        icon: "",
+        subtitle: "Health & stamina",
+        description: "Constitution measures health, stamina, and vital force.",
+        reference: "See Playing the Game: D20 Tests",
+        bullets: [
+            "Constitution checks are uncommon, and no skills apply to Constitution checks, because the endurance this ability represents is largely passive rather than involving a specific effort on the part of a character or monster. A Constitution check can model your attempt to push beyond normal limits, however.",
+            "The DM might call for a Dexterity check when you try to accomplish tasks like the following:</br><ul><li>Hold your breath</li><li>Go without sleep</li><li>Survive without food or water</li><li>Quaff an entire stein of ale in one go</li></ul>"
+        ]
+    },
+    {
+        title: "Intelligence",
+        icon: "",
+        subtitle: "Reasoning & memory",
+        description: "Intelligence measures mental acuity, accuracy of recall, and the ability to reason.",
+        reference: "See Playing the Game: D20 Tests",
+        bullets: [
+            "An Intelligence check comes into play when you need to draw on logic, education, memory, or deductive reasoning.",
+            "The DM might call for a Dexterity check when you try to accomplish tasks like the following:</br><ul><li>Communicate with a creature without using words</li><li>Estimate the value of a precious item</li><li>Recall lore about a craft or trade</li><li>Win a game of skill</li></ul>"
+        ]
+    },
+    {
+        title: "Wisdom",
+        icon: "",
+        subtitle: "Awareness, & willpower",
+        description: "Wisdom reflects how attuned you are to the world around you and represents perceptiveness and intuition.",
+        reference: "See Playing the Game: D20 Tests",
+        bullets: [
+            "A Wisdom check might reflect an effort to read body language, understand someone’s feelings, notice things about the environment, or care for an injured person.",
+            "The DM might call for a Dexterity check when you try to accomplish tasks like the following:</br><ul><li>Get a gut feeling about what course of action to follow</li><li>Discern whether a seemingly dead or living creature is undead</li></ul>"
+        ]
+    },
+    {
+        title: "Charisma",
+        icon: "",
+        subtitle: "Confidence, poise, & charm",
+        description: "Charisma measures your ability to interact effectively with others, including such factors as confidence and eloquence, and it can represent a charming or commanding personality.",
+        reference: "See Playing the Game: D20 Tests",
+        bullets: [
+            "A Charisma check might arise when you try to influence or entertain others, when you try to make an impression or tell a convincing lie, or when you are navigating a tricky social situation.",
+            "The DM might call for a Dexterity check when you try to accomplish tasks like the following:</br><ul><li>Find the best person to talk to for news, rumors, and gossip</li><li>Blend into a crowd to get the sense of key topics of conversation</li></ul>"
+        ]
+    }
+]
+
+data_d20_test_skill = [
+    {
+        title: "Acrobatics",
+        icon: "acrobatic",
+        subtitle: "Dexterity check",
+        description: "Stay on your feet in a tricky situation, or perform an acrobatic stunt.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "Your Dexterity (Acrobatics) check covers your attempt to stay on your feet in a tricky situation, such as when you’re trying to run across a sheet of ice, balance on a tightrope, or stay upright on a rocking ship’s deck. The DM might also call for a Dexterity (Acrobatics) check to see if you can perform acrobatic stunts, including dives, rolls, somersaults, and flips."
+        ]
+    },
+    {
+        title: "Animal Handling",
+        icon: "sitting-dog",
+        subtitle: "Wisdom check",
+        description: "Calm or train an animal, or get an animal to behave in a certain way.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "",
+            "Examples include the following activities:</br><ul><li>???</li><li>???</li><li>???</li></ul>"
+        ]
+    },
+    {
+        title: "Arcana",
+        icon: "secret-book",
+        subtitle: "Intelligence check",
+        description: "Recall lore about spells, magic items, and the planes of existence.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "",
+            "Examples include the following activities:</br><ul><li>???</li><li>???</li><li>???</li></ul>"
+        ]
+    },
+    {
+        title: "Athletics",
+        icon: "biceps",
+        subtitle: "Strength check",
+        description: "Jump farther than normal, stay afloat in rough water, or break something.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "Strength (Athletics) check covers difficult situations you encounter while climbing, jumping, or swimming.",
+            "Examples include the following activities:</br><ul><li>You attempt to climb a sheer or slippery cliff, avoid hazards while scaling a wall, or cling to a surface while something is trying to knock you off.</li><li>You try to jump an unusually long distance or pull off a stunt midjump.</li><li>You struggle to swim or stay afloat in treacherous currents, storm-tossed waves, or areas of thick seaweed. Or another creature tries to push or pull you underwater or otherwise interfere with your swimming.</li></ul>"
+        ]
+    },
+    {
+        title: "Deception",
+        icon: "fingers-crossed",
+        subtitle: "Charisma check",
+        description: "Tell a convincing lie, or wear a disguise convincingly.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "History",
+        icon: "archive-research",
+        subtitle: "Intelligence check",
+        description: "Recall lore about historical events, people, nations, and cultures.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Insight",
+        icon: "think",
+        subtitle: "Wisdom check",
+        description: "Discern a person’s mood and intentions.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Intimidation",
+        icon: "angry-eyes",
+        subtitle: "Charisma check",
+        description: "Awe or threaten someone into doing what you want.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Investigation",
+        icon: "sherlock-holmes",
+        subtitle: "Intelligence check",
+        description: "Find obscure information in books, or deduce how something works.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Medicine",
+        icon: "first-aid-kit",
+        subtitle: "Wisdom check",
+        description: "Diagnose an illness, or determine what killed the recently slain.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Nature",
+        icon: "oak-leaf",
+        subtitle: "Intelligence check",
+        description: "Recall lore about terrain, plants, animals, and weather.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Perception",
+        icon: "binoculars",
+        subtitle: "Wisdom check",
+        description: "Using a combination of senses, notice something that’s easy to miss.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Performance",
+        icon: "drama-masks",
+        subtitle: "Charisma check",
+        description: "Act, tell a story, perform music, or dance.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Persuasion",
+        icon: "convince",
+        subtitle: "Charisma check",
+        description: "Honestly and graciously convince someone of something.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Religion",
+        icon: "prayer",
+        subtitle: "Intelligence check",
+        description: "Recall lore about gods, religious rituals, and holy symbols.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            ""
+        ]
+    },
+    {
+        title: "Sleight of Hand",
+        icon: "magic-trick",
+        subtitle: "Dexterity check",
+        description: "Pick a pocket, conceal a handheld object, or perform legerdemain.",
+        reference: "",
+        bullets: [
+            "Whenever you attempt an act of legerdemain or manual trickery, such as planting something on someone else or concealing an object on your person, make a Dexterity (Sleight of Hand) check. The DM might also call for a Dexterity (Sleight of Hand) check to determine whether you can lift a coin purse off another person or slip something out of another person’s pocket."
+        ]
+    },
+    {
+        title: "Stealth",
+        icon: "hood",
+        subtitle: "Dexterity check",
+        description: "Escape notice by moving quietly and hiding behind things.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "Make a Dexterity (Stealth) check when you attempt to conceal yourself from enemies, slink past guards, slip away without being noticed, or sneak up on someone without being seen or heard."
+        ]
+    },
+    {
+        title: "Survival",
+        icon: "hiking",
+        subtitle: "Wisdom check",
+        description: "Follow tracks, forage, find a trail, or avoid natural hazards.",
+        reference: "See Playing the Game: Proficiency (Skill Proficiencies)",
+        bullets: [
+            "",
+            "Examples include the following activities:</br><ul><li>???</li><li>???</li><li>???</li></ul>",
+            "The DM might also call for a Strength check when you try to accomplish tasks like the following:</br><ul><li>?</li><li>?</li><li>?</li><li>?</li><li>?</li><li>?</li></ul>"
+        ]
+    }
+]
